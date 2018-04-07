8173816d278e2240ae7c6d51713a5d72841f36fa
Ming Zhou <prozm.bad@gmail.com>
2018-04-07 21:49:37 +0800

    Implementation and test case of DAE

---
+++ dae.py
@@ -0,0 +1,82 @@
+import numpy as np
+import sklearn.preprocessing as prep
+import tensorflow as tf
+
+
+def xavier_init(fan_in, fan_out, constant=1):
+    low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
+    high = constant * np.sqrt(6.0 / (fan_in + fan_out))
+    return tf.random_uniform((fan_in, fan_out), minval=low, maxval=high, dtype=tf.float32)
+
+
+# DAE中最常使用的噪声是加性高斯噪声(Additive Gaussian Noise)
+class AdditiveGaussianNoiseAutoencoder(object):
+    def __init__(self, n_input, n_hidden, transfer_function=tf.nn.softplus, optimizer=tf.train.AdamOptimizer(),
+                 scale=0.1):
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.transfer = transfer_function
+        self.scale = tf.placeholder(tf.float32)
+        self.training_scale = scale
+        network_weights = self._initialize_weights()
+        self.weights = network_weights
+        # 网络结构
+        self.x = tf.placeholder(tf.float32, [None, self.n_input])
+        self.hidden = self.transfer(tf.add(tf.matmul(self.x + scale * tf.random_normal((n_input,)), self.weights['w1']),
+                                           self.weights['b1']))
+        self.reconstruction = tf.add(tf.matmul(self.hidden, self.weights['w2']), self.weights['b2'])
+
+        # 自编码器的损失函数
+        self.cost = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction, self.x), 2.0))
+        self.optimizer = optimizer.minimize(self.cost)
+
+        init = tf.global_variables_initializer()
+        self.sess = tf.Session()
+        self.sess.run(init)
+
+    # 参数初始化函数
+    def _initialize_weights(self):
+        all_weights = dict()
+        all_weights['w1'] = tf.Variable(xavier_init(self.n_input, self.n_hidden))
+        all_weights['b1'] = tf.Variable(tf.zeros([self.n_hidden], dtype=tf.float32))
+        all_weights['w2'] = tf.Variable(tf.zeros([self.n_hidden, self.n_input], dtype=tf.float32))
+        all_weights['b2'] = tf.Variable(tf.zeros([self.n_input], dtype=tf.float32))
+        return all_weights
+
+    # 定义损失cost及执行一步训练的函数partial_fit
+    def partial_fit(self, X):
+        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.x: X, self.scale: self.training_scale})
+        return cost
+
+    # 只求损失cost的函数
+    def calc_total_cost(self, X):
+        return self.sess.run(self.cost, feed_dict={self.x: X, self.scale: self.training_scale})
+
+    def transform(self, X):
+        return self.sess.run(self.hidden, feed_dict={self.x: X, self.scale: self.training_scale})
+
+    def generate(self, hidden=None):
+        if hidden is None:
+            hidden = np.random.normal(size=self.weights["b1"])
+        return self.sess.run(self.reconstruction, feed_dict={self.hidden: hidden})
+
+    def reconstruct(self, X):
+        return self.sess.run(self.reconstruction, feed_dict={self.x: X, self.scale: self.training_scale})
+
+    def getWeights(self):
+        return self.sess.run(self.weights['w1'])
+
+    def getBiases(self):
+        return self.sess.run(self.weights['b1'])
+
+
+def standard_scale(X_train, X_test):
+    preprocessor = prep.StandardScaler().fit(X_train)
+    X_train = preprocessor.transform(X_train)
+    X_test = preprocessor.transform(X_test)
+    return X_train, X_test
+
+
+def get_random_block_from_data(data, batch_size):
+    start_index = np.random.randint(0, len(data), batch_size)
+    return data[start_index:(start_index + batch_size)]
