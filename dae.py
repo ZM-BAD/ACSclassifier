16e07ad23016b3bdf1feb8d17a6208bed49a6e69
ZM-BAD <prozm.bad@gmail.com>
2018-04-08 21:40:13 +0800

    Standardize code style

--- dae.py
+++ dae.py
@@ -44,37 +44,37 @@ class AdditiveGaussianNoiseAutoencoder(object):
         return all_weights
 
     # 定义损失cost及执行一步训练的函数partial_fit
-    def partial_fit(self, X):
-        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.x: X, self.scale: self.training_scale})
+    def partial_fit(self, x):
+        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.x: x, self.scale: self.training_scale})
         return cost
 
     # 只求损失cost的函数
-    def calc_total_cost(self, X):
-        return self.sess.run(self.cost, feed_dict={self.x: X, self.scale: self.training_scale})
+    def calc_total_cost(self, x):
+        return self.sess.run(self.cost, feed_dict={self.x: x, self.scale: self.training_scale})
 
-    def transform(self, X):
-        return self.sess.run(self.hidden, feed_dict={self.x: X, self.scale: self.training_scale})
+    def transform(self, x):
+        return self.sess.run(self.hidden, feed_dict={self.x: x, self.scale: self.training_scale})
 
     def generate(self, hidden=None):
         if hidden is None:
             hidden = np.random.normal(size=self.weights["b1"])
         return self.sess.run(self.reconstruction, feed_dict={self.hidden: hidden})
 
-    def reconstruct(self, X):
-        return self.sess.run(self.reconstruction, feed_dict={self.x: X, self.scale: self.training_scale})
+    def reconstruct(self, x):
+        return self.sess.run(self.reconstruction, feed_dict={self.x: x, self.scale: self.training_scale})
 
-    def getWeights(self):
+    def get_weights(self):
         return self.sess.run(self.weights['w1'])
 
-    def getBiases(self):
+    def get_biases(self):
         return self.sess.run(self.weights['b1'])
 
 
-def standard_scale(X_train, X_test):
-    preprocessor = prep.StandardScaler().fit(X_train)
-    X_train = preprocessor.transform(X_train)
-    X_test = preprocessor.transform(X_test)
-    return X_train, X_test
+def standard_scale(x_train, x_test):
+    preprocessor = prep.StandardScaler().fit(x_train)
+    x_train = preprocessor.transform(x_train)
+    x_test = preprocessor.transform(x_test)
+    return x_train, x_test
 
 
 def get_random_block_from_data(data, batch_size):
