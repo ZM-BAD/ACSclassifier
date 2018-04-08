c71b45667b8449344d0765f9698126a8785a2d66
Ming Zhou <prozm.bad@gmail.com>
2018-04-09 00:32:33 +0800

    UI, unfinished

--- ui_panel.py
+++ ui_panel.py
@@ -1,13 +1,22 @@
-import tkinter as tk
+from tkinter import *
+from tkinter.filedialog import askopenfilename
 
 
 class UIPanel(object):
     def __init__(self):
-        self.top = tk.Tk()
+        self.top = Tk()
         self.top.geometry("1000x600")
+        self.out_file_str = StringVar(value="select file")
+
+        self.select_file_and_predict()
 
     def show(self):
         self.top.mainloop()
+
+    # 选择数据文件并预测
+    def select_file_and_predict(self):
+        Button(self.top, text='选择', command=lambda: self.out_file_str.set(askopenfilename())).place(x=20, y=280,
+                                                                                                    anchor=W)
 
 
 if __name__ == '__main__':
