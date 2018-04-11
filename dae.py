d8e748853736f3a8131db2eedc7a5ae4e121480b
Ming Zhou <prozm.bad@gmail.com>
2018-04-11 20:40:41 +0800

    Add annotation

--- dae.py
+++ dae.py
@@ -2,7 +2,6 @@
 __author__ = 'ZM-BAD'
 
 import numpy as np
-import sklearn.preprocessing as prep
 import tensorflow as tf
 
 
@@ -10,7 +9,7 @@ def xavier_init(fan_in, fan_out, constant=1):
     """
     :param fan_in: 输入节点数量
     :param fan_out: 输出节点数量
-    :param constant:
+    :param constant: 系数常量(个人理解)
     :return:
     """
     low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
@@ -20,26 +19,32 @@ def xavier_init(fan_in, fan_out, constant=1):
 
 # DAE中最常使用的噪声是加性高斯噪声(Additive Gaussian Noise)
 class DAE(object):
-    def __init__(self, n_input, n_hidden, transfer_function=tf.nn.softplus, optimizer=tf.train.AdamOptimizer(),
+    def __init__(self, n_input, n_hidden, activation_function=tf.nn.softplus, optimizer=tf.train.AdamOptimizer(),
                  scale=0.1):
         """
         :param n_input: 输入变量数
-        :param n_hidden: 隐含层节点数
-        :param transfer_function: 隐含层激活函数
+        :param n_hidden: 隐含层节点数，即精简、抽取后的特征数
+        :param activation_function: 隐含层激活函数
         :param optimizer: 优化器
         :param scale: 高斯噪声系数
         """
         self.n_input = n_input
         self.n_hidden = n_hidden
-        self.transfer = transfer_function
+        self.activation = activation_function
         self.scale = tf.placeholder(tf.float32)
         self.training_scale = scale
-        network_weights = self._initialize_weights()
+        network_weights = self.initialize_weights()
         self.weights = network_weights
+
         # 网络结构
         self.x = tf.placeholder(tf.float32, [None, self.n_input])
-        self.hidden = self.transfer(tf.add(tf.matmul(self.x + scale * tf.random_normal((n_input,)), self.weights['w1']),
-                                           self.weights['b1']))
+        # w1 隐含层的权重
+        # b1 隐含层的偏置
+        # w2 输出层的权重
+        # b2 输出层的偏置
+        self.hidden = self.activation(
+            tf.add(tf.matmul(self.x + scale * tf.random_normal((n_input,)), self.weights['w1']),
+                   self.weights['b1']))
         self.reconstruction = tf.add(tf.matmul(self.hidden, self.weights['w2']), self.weights['b2'])
 
         # 自编码器的损失函数
@@ -50,8 +55,8 @@ class DAE(object):
         self.sess = tf.Session()
         self.sess.run(init)
 
-    # 参数初始化函数
-    def _initialize_weights(self):
+    # 参数初始化函数，要初始化4个参数，w1，b1，w2，b2
+    def initialize_weights(self):
         all_weights = dict()
         all_weights['w1'] = tf.Variable(xavier_init(self.n_input, self.n_hidden))
         all_weights['b1'] = tf.Variable(tf.zeros([self.n_hidden], dtype=tf.float32))
@@ -68,6 +73,7 @@ class DAE(object):
     def calc_total_cost(self, x):
         return self.sess.run(self.cost, feed_dict={self.x: x, self.scale: self.training_scale})
 
+    # 整个DAE其实就两点，transform提取特征，generate通过高阶特征还原数据
     def transform(self, x):
         return self.sess.run(self.hidden, feed_dict={self.x: x, self.scale: self.training_scale})
 
@@ -79,20 +85,10 @@ class DAE(object):
     def reconstruct(self, x):
         return self.sess.run(self.reconstruction, feed_dict={self.x: x, self.scale: self.training_scale})
 
+    # 获取隐藏层权重w1
     def get_weights(self):
         return self.sess.run(self.weights['w1'])
 
+    # 获取隐藏层偏置b1
     def get_biases(self):
         return self.sess.run(self.weights['b1'])
-
-
-def standard_scale(x_train, x_test):
-    preprocessor = prep.StandardScaler().fit(x_train)
-    x_train = preprocessor.transform(x_train)
-    x_test = preprocessor.transform(x_test)
-    return x_train, x_test
-
-
-def get_random_block_from_data(data, batch_size):
-    start_index = np.random.randint(0, len(data), batch_size)
-    return data[start_index:(start_index + batch_size)]
