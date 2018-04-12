1cbeed36a2b86db107c41159e7d6c48fa763314f
Ming Zhou <prozm.bad@gmail.com>
2018-04-13 00:39:15 +0800

    Init

---
+++ control.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = 'ZM-BAD'
+"""
+UI界面与模型之间的交互操作都包含在本模块中
+"""
+
+
+# 将选择的文件的路径传到函数中，返回f1_score等结果
+def calc_numerical_result(dataset_file_path):
+    f1_score = "f1_score"
+    precision = "precision"
+    recall = "recall"
+    return f1_score, precision, recall
