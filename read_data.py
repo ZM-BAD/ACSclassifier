5873faebcecdde7c677523eefcd6fc01c69d859a
Ming Zhou <prozm.bad@gmail.com>
2018-04-17 19:32:52 +0800

    Change labels

--- read_data.py
+++ read_data.py
@@ -4,32 +4,26 @@ __author__ = 'ZM-BAD'
 import numpy as np
 import csv
 
-filename = 'dataset.csv'
-csvFile = open(filename, 'rb')
-NUM_OF_SAMPLE = len(csvFile.readlines()) - 1  # 样本总数为.csv文件行数-1
-NUM_OF_FEATURE = 442  # feature总数为442个
+# dataset.csv文件中一共有
+# 2930个数据样本
+# 442个feature
 
-input_tensor = np.zeros([NUM_OF_SAMPLE, NUM_OF_FEATURE])
-bleeding = np.zeros([NUM_OF_SAMPLE, 1])
-ischemic = np.zeros([NUM_OF_SAMPLE, 1])
+reader = csv.reader(open('./resource/dataset.csv', encoding='gbk'))
+data = np.zeros([2930, 444])  # 储存全部的数据，先把所有的数据都读进来
+label = np.zeros([2930, 4])  # 所有样本的标签
 
-reader = csv.reader(open(filename, encoding='utf-8'))
-
-# 将数据写入input_tensor，注意表格中的第一行和前两列
-lineNo = -1
+line = -1
 for row in reader:
-    lineNo += 1
-    if lineNo > 0:
-        input_tensor[lineNo - 1:lineNo, 0:NUM_OF_FEATURE] = row[2:444]
+    line += 1
+    if line > 0:
+        data[line - 1, 0:444] = row[0:444]
 
-# 将label值存入bleeding和ischemic
-for i in range(NUM_OF_SAMPLE):
-    if input_tensor[i:i + 1, 7:8] == 1:  # 如果有缺血事件发生则第1位写上1，没有在第0位上写上1
-        ischemic[i:i + 1, 1:2] = 1
-    else:
-        ischemic[i:i + 1, 0:1] = 1
-
-    if input_tensor[i:i + 1, 8:9] == 1:  # 如果有出血事件发生则第1位写上1，没有在第0位上写上1
-        bleeding[i:i + 1, 1:2] = 1
-    else:
-        bleeding[i:i + 1, 0:1] = 1
+for i in range(2930):
+    if data[i, 0] == 0 and data[i, 1] == 0:         # 不缺血，不出血
+        label[i, 0] = 1
+    elif data[i, 0] == 0 and data[i, 1] == 1:       # 不缺血，出血
+        label[i, 1] = 1
+    elif data[i, 0] == 1 and data[i, 1] == 0:       # 缺血，不出血
+        label[i, 2] = 1
+    else:                                           # 缺血，出血
+        label[i, 3] = 1
