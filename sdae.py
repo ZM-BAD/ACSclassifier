37ef1212f16dc0c634dc4aaa5da52b33db3a8f20
ZM-BAD <prozm.bad@gmail.com>
2018-04-18 03:02:35 +0800

    Fix bug

--- sdae.py
+++ sdae.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ZM-BAD'
 
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+
 from dae import DAE
 
 
 # SDAE是基于DAE实现的，其中DAE使用的噪声为加性高斯噪声
 class SDAE(object):
     def __init__(self, n_input, hiddens, transfer_function=tf.nn.softplus, scale=0.1, name='sdae', sess=None,
-                 optimizer=tf.train.AdamOptimizer(), epochs=1000):
+                 optimizer=tf.train.AdamOptimizer()):
         """
         :param n_input: 输入节点数
         :param hiddens: 每个隐藏层中的神经元数，是一个list
         :param transfer_function: transfer(activation) function
         :param scale: 高斯噪声系数
         :param optimizer: 优化器
-        :param epochs: 训练的迭代轮数
         :param name: 命名
         :param sess: 会话，加入此参数用来确保所有的DAE都被同一个Session管理
         """
@@ -25,7 +25,6 @@ class SDAE(object):
             self.n_input = n_input
             self.stacks = len(hiddens)
             self.scale = scale
-            self.epochs = epochs
             self.transfer = transfer_function
             self.optimizer = optimizer
             self.sess = sess if sess is not None else tf.Session()
@@ -35,7 +34,7 @@ class SDAE(object):
             self.x = tf.placeholder(tf.float32, [None, n_input], name="input")
             self.hidden = self.x
             for dae in self.sdae:
-                self.hidden = dae.decode_func(self.hidden)
+                self.hidden = dae.encode_func(self.hidden)
 
             self.rec = self.hidden
             for dae in reversed(self.sdae):
@@ -43,18 +42,6 @@ class SDAE(object):
 
             init = tf.global_variables_initializer()
             self.sess.run(init)
-
-    # def __call__(self, x):
-    #     """
-    #     as a component of parent model
-    #
-    #     :param x: input tensor
-    #     :return: hidden representation tensor
-    #     """
-    #     x_copy = x
-    #     for dae in self.sdae:
-    #         x_copy = dae(x_copy)
-    #     return x_copy
 
     def _init_sdae(self, n_input, hiddens):
         """
@@ -83,18 +70,26 @@ class SDAE(object):
                 stacked_dae.append(dae)
         return stacked_dae
 
-    def pre_train(self, data_set, batch_size=128):
+    def pre_train(self, train_data, epochs=1000):
         """
-        预训练模型，所谓"预"是针对提取特征之后的训练
-        :param data.read_data.DataSet data_set: the training data set
-        :param batch_size: `batch size` of data
+        对SDAE进行预训练。所谓'预'是相对于抽取特征之后的训练而言。函数作用类似于DAE中的partial_fit
+        :param train_data: 用于训练的数据，是二阶张量
+        :param epochs: epoch of training
+        :return: return nothing
         """
-        for i in range(self.stacks):
-            while data_set.epoch_completed < self.epochs:
-                x, _ = data_set.next_batch(batch_size)
-                self.sdae[i].partial_fit(x)
-            x = self.sdae[i].encode(data_set.examples)
-            data_set = DataSet(x, data_set.labels)
+        temp_train = train_data
+        for index in range(self.stacks):
+            for i in range(epochs):
+                self.sdae[index].partial_fit(temp_train)
+            temp_train = self.sdae[index].encode_func(temp_train).eval(session=self.sess)
+
+    # def pre_train(self, data_set, batch_size=128):
+    #     for i in range(self.stacks):
+    #         while data_set.epoch_completed < self.epochs:
+    #             x, _ = data_set.next_batch(batch_size)
+    #             self.sdae[i].partial_fit(x)
+    #         x = self.sdae[i].encode(data_set.examples)
+    #         data_set = DataSet(x, data_set.labels)
 
     def encode(self, x):
         """
@@ -137,10 +132,8 @@ if __name__ == "__main__":
                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1]])
-    text_x = np.array([[1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    test_x = np.array([[1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1]])
-    data = DataSet(train_x, np.arange(train_x.shape[0]))
-    sdae.pre_train(data, 10)
-
-    rec_x = sdae.reconstruct(text_x)
+    sdae.pre_train(train_x)
+    rec_x = sdae.reconstruct(test_x)
     print(rec_x)
